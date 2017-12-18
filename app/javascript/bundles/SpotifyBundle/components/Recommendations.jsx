74633fce8ea890b23abe2a397ade2534e443dbf9
RobertJHog <rj.hogerbrugge@hotmail.com>
2017-12-18 08:51:29 +0100

    recommendations component + filter

--- app/javascript/bundles/SpotifyBundle/components/Recommendations.jsx
+++ app/javascript/bundles/SpotifyBundle/components/Recommendations.jsx
@@ -2,22 +2,43 @@ import React, { Component } from 'react'
 import Title from './Title'
 import { connect } from 'react-redux'
 import RecommendationItem from './RecommendationItem'
-import { Box } from 'reactbulma'
+import { Box, Button } from 'reactbulma'
 import './Recommendations.scss'
 
 class Recommendations extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
   renderRecommendations(recommendation, index) {
     return (
       <RecommendationItem key={index} {...recommendation} />
     )
   }
 
+  handleClick() {
+    const recommendationsArray = []
+    this.props.toptracks.map(function (toptrack) {
+      if (toptrack.liked === true) {
+        recommendationsArray.push(toptrack)
+      } else {
+        return
+      }
+    })
+    console.log(recommendationsArray)
+    // return recommendationsArray
+  }
+
+  // API call to spotify with recommendationsArray
+
   render() {
     return(
       <div className="container recommendations">
         <div className="songs wrapper">
           <header>
-            <Title content="Recommendations" />
+            <Button onClick={this.handleClick} primary>Get Recommendations</Button>
           </header>
           <main className="container recommendations-overview">
             { this.props.recommendations.map(this.renderRecommendations) }
@@ -28,6 +49,6 @@ class Recommendations extends React.Component {
   }
 }
 
-const mapStateToProps = ({ recommendations }) => ({ recommendations })
+const mapStateToProps = ({ recommendations, toptracks }) => ({ recommendations, toptracks })
 
 export default connect(mapStateToProps)(Recommendations)
