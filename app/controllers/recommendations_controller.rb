fbf54503055aba01219e9c3e92f9e282d285d3ed
RobertJHog <rj.hogerbrugge@hotmail.com>
2017-12-30 11:41:27 +0100

    Added feedback to inactive likebutton, repositioned recommendations, no recommendations and changed layout toptrack item. Already added some methods in rec controller to add to playlist (not finished)

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -3,8 +3,20 @@ class RecommendationsController < ApplicationController
 
   def index
     spotify_user = RSpotify::User.new(session[:spotify_access_token])
-    @recommendations = RSpotify::Recommendations.generate(seed_tracks: params[:seeds_array])
-    render json: @recommendations, status: :ok
+    recommendations = RSpotify::Recommendations.generate(seed_tracks: params[:seeds_array])
+    render json: recommendations, status: :ok
+  end
+
+  def createplaylist
+    # Create playlist in user's Spotify account
+    playlist = spotify_user.create_playlist!('my-songadvisor-playlist')
+  end
+
+  def addtoplaylist
+    # Add tracks to a playlist in user's Spotify account
+    track = RSpotify::Track.search('Know')
+    playlist.add_tracks!(track)
+    playlist.tracks.first.name #=> "Somebody That I Used To Know"
   end
 
 end
